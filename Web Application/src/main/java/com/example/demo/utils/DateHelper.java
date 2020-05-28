588412b883c3e60ce576935022b53c8ad89d4800
Museberg <Rasmus@falk-jensen.dk>
2020-05-28 15:26:18 +0200

    Updated name of daysBetween to hoursBetween

--- Web Application/src/main/java/com/example/demo/utils/DateHelper.java
+++ Web Application/src/main/java/com/example/demo/utils/DateHelper.java
@@ -8,7 +8,7 @@ import java.time.temporal.ChronoUnit;
 
 public class DateHelper {
 
-    public static int daysBetween(String startDate, String endDate){
+    public static int hoursBetween(String startDate, String endDate){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime formattedStartDate = LocalDateTime.parse(startDate, formatter);
         LocalDateTime formattedEndDate = LocalDateTime.parse(endDate, formatter);
@@ -20,9 +20,9 @@ public class DateHelper {
         startDate2 = Reservation.fixDateFormatting(startDate2);
         endDate1 = Reservation.fixDateFormatting(endDate1);
         endDate2 = Reservation.fixDateFormatting(endDate2);
-        int daysBetweenStart1End2 = daysBetween(startDate1, endDate2);
+        int daysBetweenStart1End2 = hoursBetween(startDate1, endDate2);
         System.out.println("Days between start 1 and end 2: " + daysBetweenStart1End2);
-        int daysBetweenStart2End1 = daysBetween(startDate2, endDate1);
+        int daysBetweenStart2End1 = hoursBetween(startDate2, endDate1);
         System.out.println("Days between start 2 end 1: " + daysBetweenStart2End1);
         return (daysBetweenStart1End2 >= 0 && daysBetweenStart2End1 >= 0);
         //( start1 <= end2 and start2 <= end1 )
